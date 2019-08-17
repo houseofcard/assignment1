1495156d7ea4520e44be67bdd7a7d44c0e43d840
houseofcard <53417201+houseofcard@users.noreply.github.com>
2019-08-18 11:44:39 +1200

    Update index to create select box and link to gothic.php within its own VM and www directory

--- www/index.php
+++ www/index.php
@@ -13,6 +13,16 @@ th, td {
   padding: 0.2em;
 }
 </style>
+<?php
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+?>
 </head>
 
 <body>
@@ -24,16 +34,6 @@ th, td {
 <tr><th>Window Code</th><th>Window Name</th></tr>
 
 <?php
- 
-$db_host   = '192.168.2.12';
-$db_name   = 'fvision';
-$db_user   = 'webuser';
-$db_passwd = 'insecure_db_pw';
-
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-
 $q = $pdo->query("SELECT * FROM stainedwindows");
 
 while($row = $q->fetch()){
@@ -42,5 +42,44 @@ while($row = $q->fetch()){
 
 ?>
 </table>
-</body>
+
+<p>Select Stained Glass Window</p>
+
+<form action="#" method="post">
+<select name="windows">
+<?php
+$q = $pdo->query("SELECT * FROM stainedwindows");
+    
+    while($row = $q->fetch()){
+        echo "<option value = ".$row["id"].">".$row["name"]."</option>\n";
+    }
+    ?>
+</select>
+<input type="submit" name="submit" value="Get Selected Value"/>
+</form>
+<?php
+    if(isset($_POST['submit'])){
+        $selected_val = $_POST['windows'];  // Storing Selected Value In Variable
+        echo "You have selected :" .$selected_val;  // Displaying Selected Value
+    }
+    ?>
+<br>
+
+<?php
+    if ($selected_val == '1'){
+        echo "<a href='astrological.php'>Astrological</a>";
+    }
+    if ($selected_val == '2'){
+        echo "<a href='coloured_tree.php'>Coloured Tree</a>";
+    }
+    if ($selected_val == '3'){
+        echo "<a href='gothic.php'>Gothic</a>";
+    }
+    if ($selected_val == '4'){
+        echo "<a href='red_plant.php'>Red Plant</a>";
+    }
+    if ($selected_val == '5'){
+        echo "<a href='religious.php'>Religious</a>";
+    }
+    ?></body>
 </html>
